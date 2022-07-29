2c683833d93b1971a5601b08e695a7188871914b
DESKTOP-PFQQR2S\quwer <quwert86@gmail.com>
2022-07-29 20:52:04 +0300

    Program was added

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// 1 четверть
+
+
+string[] array1 = new string[5] {"I", "wanna", "get", "excellent","result"};
+string[] array2 = new string[array1.Length];
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i=i+1)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i=i+1)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(array1, array2);
+PrintArray(array2);
